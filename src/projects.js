88174257efa5c09b630ca440c227610c403760d2
Lucian Cheng <luciancheng3@gmail.com>
2024-05-07 21:20:56 -0400

    update

--- src/projects.js
+++ src/projects.js
@@ -8,7 +8,7 @@ import dimplecircuit from "./images/dimplecircuit.png";
 
 const Projects = () => {
     const projects = [
-    {title: "Portfolio Website", desc: "Built a React JS portoflio website to display my experiences and projects in a visually appealing manner", skills: ["html", "css", "js", "react"], links: "https://", img: webdev},
+    {title: "Portfolio Website", desc: "Built a React JS portoflio website to display my experiences and projects in a visually appealing manner", skills: ["html", "css", "js", "react"], links: "https://github.com/luciancheng/LucianChengPortfolio", img: webdev},
     {title: "Smart Baby Monitor Device", desc: "Developed a prototype and verification plan for a smart baby monitor device to process frequencies of cries to detect the issue. Built with a filter circuit, Python and a MongoDB AWS database.", skills: ["python", "mongodb", "aws"], links: "https://", img: dimplecircuit}, 
     {title: "Embedded Systems Design", desc: "Developed many embedded systems circuits using a microcontroller and actuators built with circuit components and logic", skills: ["cpp"], links: "https://github.com/luciancheng/Cpp-Projects/tree/main/Embedded-Systems", img: embeddedsystem},
     {title: "Pacemaker Project", desc: "Created a pacemaker medical device with a Python Tkinter GUI and physical device. The GUI displayed graph data  and allowed for modificaiton of control variables and parameters.", skills: ["python"], links: "https://github.com/luciancheng/PacemakerProject", img: pacemaker},
