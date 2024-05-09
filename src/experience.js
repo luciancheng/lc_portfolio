6d65d2563b58ee5e30812eed6198fa6da45c268a
Lucian Cheng <luciancheng3@gmail.com>
2024-05-08 20:47:24 -0400

    stuff

--- src/experience.js
+++ src/experience.js
@@ -1,7 +1,7 @@
 import TimelineContainer from "./timelineContainerTemplate";
 
 const Experience = () => {
-    const experiences = [{company: "Bluewrist Inc.", position: "Software Engineering Intern Co-op", time: "May 2024 - Present", desc: "Designed the pipeline and infrastructure for scalable ML model deployment using flask and AWS Sagemaker.", skills: ["python", "aws", "pytorch", "cpp", "docker"], side: "left"},
+    const experiences = [{company: "Bluewrist Inc.", position: "Software Engineering Intern Co-op", time: "May 2024 - Present", desc: "Designed the pipeline and infrastructure for scalable ML model training and deployment for customers using VB.NET and a API calls to a local Python server with Flask.", skills: ["python","pytorch", "cpp", "flask"], side: "left"},
 {company: "Bluewrist Inc.", position: "Software Engineering Intern Co-op", time: "May 2023 - August 2024", desc: "Implemented the Pointnet++ part segmenetation ML model using pytorch for identification of weld surfaces from 3D point cloud scans with +98% accuracy.", skills: ["python", "pytorch", "cpp"], side: "right"},
 {company: "McMaster EcoCAR EV Challenge", position: "CAVs General Member", time: "October 2022 - Present", desc: "Collaborated in a team to develop autnomous driving algorithms using sensor data from the vehicle.", skills: ["python"], side: "left"},
 {company: "McMaster MED-T Guided Hands", position: "Technical Writer", time: "October 2022 - December 2022", desc: "Transcribed code in C# to readable and understandable code for investors and other stakeholders.", skills: ["csharp"], side: "right"},
